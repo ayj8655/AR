31cffa1ebf67dce70159a50e6ab85e2454e41bbc
AhnYoungJin <ayj8655@naver.com>
2019-04-09 23:46:38 +0900

    use mapbax + GPS

--- app/src/main/java/com/example/ar/LoginActivity.java
+++ app/src/main/java/com/example/ar/LoginActivity.java
@@ -1,30 +1,128 @@
 package com.example.ar;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+
 
 
 //로그인
 public class LoginActivity extends AppCompatActivity {
+    private Button btnShowLocation;
+    private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
+    private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
+    private boolean isAccessFineLocation = false;
+    private boolean isAccessCoarseLocation = false;
+    private boolean isPermission = false;
+    // GPSTracker class
+    private GpsInfo gps;
+
+    public static final int REQUEST_CODE_MAIN=101;
+
+    double latitude;
+    double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
 
-    public void Login_onClick(View v){  //온클릭으로 인텐트
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
+        btnShowLocation = (Button) findViewById(R.id.Login_button);
+
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // 권한 요청을 해야 함
+                if (!isPermission) {
+                    callPermission();
+                    return;
+                }
+                gps = new GpsInfo(LoginActivity.this);
+                // GPS 사용유무 가져오기
+                if (gps.isGetLocation()) {
+                    latitude = gps.getLatitude();
+                    longitude = gps.getLongitude();
+                    //Toast.makeText(
+                      //      getApplicationContext(),
+                      //      "당신의 위치 - \n위도: " + latitude + "\n경도: " + longitude,
+                      //      Toast.LENGTH_LONG).show();
+                } else {
+                    // GPS 를 사용할수 없으므로
+                    gps.showSettingsAlert();
+                }
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("위도", latitude);
+                intent.putExtra("경도", longitude);
+                startActivity(intent);
+/*
+                Intent intent = getIntent();
+                Double latitude = intent.getExtras().getDouble("위도");
+                Double longitude= intent.getExtras().getDouble("경도");
+
+*/
+
+            }
+        });
+        callPermission();  // 권한 요청을 해야 함
     }
+
 
     public void Signup_onClick(View v){
         Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
         startActivity(intent);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_ACCESS_FINE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            isAccessFineLocation = true;
+
+        } else if (requestCode == PERMISSIONS_ACCESS_COARSE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            isAccessCoarseLocation = true;
+        }
+
+        if (isAccessFineLocation && isAccessCoarseLocation) {
+            isPermission = true;
+        }
+    }
+
+    // 전화번호 권한 요청
+    private void callPermission() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_ACCESS_FINE_LOCATION);
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_ACCESS_COARSE_LOCATION);
+        } else {
+            isPermission = true;
+        }
     }
 
 
--- app/src/main/java/com/example/ar/MainActivity.java
+++ app/src/main/java/com/example/ar/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ar;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Address;
@@ -16,6 +18,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -29,15 +32,13 @@ import com.mapbox.geojson.LineString;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.Polyline;
 import com.mapbox.mapboxsdk.annotations.PolylineOptions;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.skt.Tmap.TMapGpsManager;
-import com.skt.Tmap.TMapPoint;
-import com.skt.Tmap.TMapView;
 
 import java.io.IOException;
 import java.lang.reflect.Array;
@@ -61,18 +62,14 @@ public class MainActivity extends AppCompatActivity {
     private MapboxMap map;
     private DirectionsRoute currentRoute;
     private MapboxDirections client;
+    Double latitude;
+    Double longitude;
+    EditText editText;
+
     double destinationX; // longitude
     double destinationY; // latitude
 
-
-    private Button btnShowLocation;
-    private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
-    private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
-    private boolean isAccessFineLocation = false;
-    private boolean isAccessCoarseLocation = false;
-    private boolean isPermission = false;
-    // GPSTracker class
-    private GpsInfo gps;
+    Polyline mPolyline;
 
 //288
 
@@ -81,44 +78,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = getIntent();
+        latitude= intent.getExtras().getDouble("위도");
+        longitude= intent.getExtras().getDouble("경도");
 
-        btnShowLocation = (Button) findViewById(R.id.btn_start);
-
-
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                // 권한 요청을 해야 함
-                if (!isPermission) {
-                    callPermission();
-                    return;
-                }
-                gps = new GpsInfo(MainActivity.this);
-                // GPS 사용유무 가져오기
-                if (gps.isGetLocation()) {
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-                    Toast.makeText(
-                            getApplicationContext(),
-                            "당신의 위치 - \n위도: " + latitude + "\n경도: " + longitude,
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    // GPS 를 사용할수 없으므로
-                    gps.showSettingsAlert();
-                }
-            }
-        });
-
-        callPermission();  // 권한 요청을 해야 함
-
+        editText =(EditText)findViewById(R.id.editText11);
 
         // 맵박스 사용하기 위한 접근 토큰 지정
         Mapbox.getInstance(this, getString(R.string.access_token));
         // 아래 함수로 통해 목적지 주소값을 위도 경도 값으로 변경
-        getPointFromGeoCoder("충청남도 천안시 동남구 단대로 119");
+   //     getPointFromGeoCoder("null");
         // 사용자 현재 gps 위치
-        final Point origin = Point.fromLngLat(127.17937, 36.833038);
+        final Point origin = Point.fromLngLat(longitude, latitude);
         // 도착지 gps 위치
-        final Point destination = Point.fromLngLat(destinationX, destinationY);
+   //     final Point destination = Point.fromLngLat(destinationX, destinationY);
         // Setup the MapView
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
@@ -130,31 +103,17 @@ public class MainActivity extends AppCompatActivity {
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(
                         // 카메라는 반대의 값으로 적어줄 것
                         // 뒤에 숫자 15은 카메라 확대 배수이다( 15가 적당 )
-                        new LatLng(36.833038, 127.17937), 12));
+                        new LatLng(latitude, longitude), 12));
                 // Add origin and destination to the map
                 mapboxMap.addMarker(new MarkerOptions()
                         .position(new LatLng(origin.latitude(), origin.longitude()))
                         // 타이틀은 상호명 건물명, snippet은 설명 그에 대한 설명이다
                         // 출발지
                         //.title("소우")
-                        .snippet("출발지"));
-                mapboxMap.addMarker(new MarkerOptions()
-                        // 목적지
-                        .position(new LatLng(destination.latitude(), destination.longitude()))
-                        //.title("가비")
-                        .snippet("도착지"));
-                // Get route from API
-                getRoute(origin, destination);
+                        .snippet("현재 위치"));
             }
         });
     }
-
-
-
-
-
-
-
     private void getRoute(Point origin, Point destination) {
 
         client = MapboxDirections.builder()
@@ -196,6 +155,21 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // 목적지 주소값을 통해 목적지 위도 경도를 얻어오는 구문
+    public void getPointFromGeoCoder(String addr) {
+        String destinationAddr = addr;
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> listAddress = null;
+        try {
+            listAddress = geocoder.getFromLocationName(destinationAddr, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        destinationX = listAddress.get(0).getLongitude();
+        destinationY = listAddress.get(0).getLatitude();
+        System.out.println( addr + "'s Destination x, y = " + destinationX + ", " + destinationY);
+    }
+
     private void drawRoute(DirectionsRoute route) {
         // Convert LineString coordinates into LatLng[]
         LineString lineString = LineString.fromPolyline(route.geometry(), PRECISION_6);
@@ -206,12 +180,8 @@ public class MainActivity extends AppCompatActivity {
                     coordinates.get(i).latitude(),
                     coordinates.get(i).longitude());
         }
-
         // Draw Points on MapView
-        map.addPolyline(new PolylineOptions()
-                .add(points)
-                .color(Color.parseColor("#009688"))
-                .width(5));
+        map.addPolyline(new PolylineOptions().add(points).color(Color.parseColor("#009688")).width(5));
     }
 
     @Override
@@ -260,66 +230,19 @@ public class MainActivity extends AppCompatActivity {
         mapView.onLowMemory();
     }
 
-    // 목적지 주소값을 통해 목적지 위도 경도를 얻어오는 구문
-    public void getPointFromGeoCoder(String addr) {
-        String destinationAddr = addr;
-
-        Geocoder geocoder = new Geocoder(this);
-        List<Address> listAddress = null;
-        try {
-            listAddress = geocoder.getFromLocationName(destinationAddr, 1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        destinationX = listAddress.get(0).getLongitude();
-        destinationY = listAddress.get(0).getLatitude();
-
-        System.out.println( addr + "'s Destination x, y = " + destinationX + ", " + destinationY);
+    public void newmap(View view) {
+        // 카메라 위치 고정(내 gps 위치로 임의지정)
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                // 카메라는 반대의 값으로 적어줄 것
+                // 뒤에 숫자 15은 카메라 확대 배수이다( 15가 적당 )
+                new LatLng(latitude, longitude), 12));
     }
 
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                           int[] grantResults) {
-        if (requestCode == PERMISSIONS_ACCESS_FINE_LOCATION
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-            isAccessFineLocation = true;
-
-        } else if (requestCode == PERMISSIONS_ACCESS_COARSE_LOCATION
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
-            isAccessCoarseLocation = true;
-        }
-
-        if (isAccessFineLocation && isAccessCoarseLocation) {
-            isPermission = true;
-        }
+    public void map_search(View view) {
+        Toast.makeText(getApplicationContext(),editText.getText().toString(), Toast.LENGTH_LONG).show();
+        getPointFromGeoCoder(editText.getText().toString());
+        final Point origin = Point.fromLngLat(longitude, latitude);
+        final Point destination = Point.fromLngLat(destinationX, destinationY);
+        getRoute(origin,destination);
     }
-
-    // 전화번호 권한 요청
-    private void callPermission() {
-        // Check the SDK version and whether the permission is already granted or not.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            requestPermissions(
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_ACCESS_FINE_LOCATION);
-
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
-
-            requestPermissions(
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                    PERMISSIONS_ACCESS_COARSE_LOCATION);
-        } else {
-            isPermission = true;
-        }
-    }
-
-
 }
