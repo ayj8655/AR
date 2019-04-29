3a7a943f6b41f7baa1eb63c40bdf185cd87e42ba
taeyoung <119taeyoung@naver.com>
2019-04-29 19:56:49 +0900

    add facebook login api

--- app/src/main/java/com/example/ar/LoginActivity.java
+++ app/src/main/java/com/example/ar/LoginActivity.java
@@ -3,20 +3,42 @@ package com.example.ar;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 
 //로그인
 public class LoginActivity extends AppCompatActivity {
+
+    // facebook
+    CallbackManager callbackManager;
+
+    // mapbox
     private Button btnShowLocation;
     private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
     private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
@@ -36,7 +58,67 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // facebook
+        printKeyHash();
 
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.d("response", response.toString());
+
+                        try{
+                            String email = object.getString("email");
+                            String name = object.getString("name");
+                            String gender = object.getString("gender");
+
+                            Log.d("TAG", "페이스북 이메일→"+email);
+                            Log.d("TAG", "페이스북 이름→"+name);
+                            Log.d("TAG", "페이스북 성별→"+gender);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+        // If already login
+        if(AccessToken.getCurrentAccessToken() != null) {
+            // Just set User ID
+
+        }
+        // .facebook
+
+        // mapbox
         btnShowLocation = (Button) findViewById(R.id.Login_button);
 
         btnShowLocation.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +156,39 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
         callPermission();  // 권한 요청을 해야 함
+        // .mapbox
+    }
+
+    // facebook
+    private void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.ar", PackageManager.GET_SIGNATURES);
+            for(Signature signature:info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash ", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void facebook_OnClick(View view) {
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    // .facebook
+
+    public void Login_onClick(View v){  //온클릭으로 인텐트
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 
 
@@ -82,7 +197,7 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
+    // mapbox
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions,
                                            int[] grantResults) {
@@ -124,6 +239,6 @@ public class LoginActivity extends AppCompatActivity {
             isPermission = true;
         }
     }
-
+    // .mapbox
 
 }
